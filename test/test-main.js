bf29541d8c51cabd81c6f74a6660e7dc0eb56cb6
cat_in_136 <cat.in.136+github@gmail.com>
2012-09-17 18:13:40 +0900

    fix typo

--- test/test-main.js
+++ test/test-main.js
@@ -1,32 +1,20 @@
 const main = require("main");
 
-exports.test_test_run = function(test) {
-  test.pass("Unit test running!");
+exports["test main and onUnload"] = function(test) {
+  let options = {
+    id: require("self").id,
+    name: require("self").name,
+    version: require("self").version,
+  };
+
+  options.loadReason = "install";
+  main.main(options, null);
+  main.onUnload("disable");
+
+  main.main(options, null);
+  main.onUnload("disable");
+
+  test.pass();
 };
 
-exports.test_id = function(test) {
-  test.assert(require("self").id.length > 0);
-};
-
-exports.test_url = function(test) {
-  require("request").Request({
-    url: "http://www.mozilla.org/",
-    onComplete: function(response) {
-      test.assertEqual(response.statusText, "OK");
-      test.done();
-    }
-  }).get();
-  test.waitUntilDone(20000);
-};
-
-exports.test_open_tab = function(test) {
-  const tabs = require("tabs");
-  tabs.open({
-    url: "http://www.mozilla.org/",
-    onReady: function(tab) {
-      test.assertEqual(tab.url, "http://www.mozilla.org/");
-      test.done();
-    }
-  });
-  test.waitUntilDone(20000);
-};
+require("test").run(exports);
